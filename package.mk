b0e8bc27c0e114e4778aa211cb03d75bd248eeff
David Wragg <david@rabbitmq.com>
2010-12-02 12:32:35 +0000

    Overhaul for bug23568

--- package.mk
+++ package.mk
@@ -1,4 +1,3 @@
 RELEASABLE:=true
-
 DEPS:=rabbitmq-server rabbitmq-erlang-client erlang-rfc4627-wrapper rabbitmq-mochiweb mochiweb-wrapper rabbitmq-jsonrpc
-EXTRA_PACKAGE_DIRS:=priv
+EXTRA_PACKAGE_DIRS:=$(PACKAGE_DIR)/priv
